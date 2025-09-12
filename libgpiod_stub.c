3609d93811fc49fd749a9be9eacd6392f5dc6f2a
Tyler Rider <tylerjrider@gmail.com>
2025-09-12 17:27:27 -0400

    xy-pen-plotter: Add libgpiod stubs to build off target

--- libgpiod_stub.c
+++ libgpiod_stub.c
@@ -1,12 +1,8 @@
-
-
-// libgpiod stubs.
 #include <stdio.h>
 #include <stdlib.h>
 
 struct gpiod_chip { const char* p;};
 struct gpiod_line { int n;};
-
 
 struct gpiod_chip* gpiod_chip_open(const char *path) {
     fprintf(stderr, "STUB: gpiod_chip_open(%s)\n", path);
@@ -23,12 +19,16 @@ void gpiod_chip_close(struct gpiod_chip *chip)
 
 int gpiod_line_set_value(struct gpiod_line *line, int value)
 {
-    fprintf(stderr, "STUB: %s val: %d\n", __func__, value);
+    fprintf(stderr, "STUB: %s pin:%d val: %d\n", __func__, line->n, value);
+    return 0;
 }
 
-struct gpiod_line* gpiod_chip_get_line(struct gpiod_chip*, int)
+struct gpiod_line* gpiod_chip_get_line(struct gpiod_chip*, int n )
 {
-    return (struct gpiod_line*)malloc(sizeof(struct gpiod_line));
+    struct gpiod_line* p =
+        (struct gpiod_line*) malloc(sizeof(struct gpiod_line));
+    p->n = n;
+    return p; 
 }
 
 int gpiod_line_request_output(struct gpiod_line*, const char*, int)
